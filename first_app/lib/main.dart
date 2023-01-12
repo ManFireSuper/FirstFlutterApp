8988bea11ccc5bd1981365ba258e599ccde36e3b
Manfiresuper <ekpreetsingh1st@gmail.com>
2023-01-12 17:00:07 +0530

    Day1

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
+          body: Center(
+              child: Text("This is an text",
+                  style: TextStyle(color: Color(0xFFFF0000)))),
+          appBar: AppBar(title: Text("This is an title")))));
+}
+
+// Day1
+/*
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
           body: Center(child: Text("This is a test field")),
           appBar: AppBar(title: Text("New Title")))));
 }
+*/
