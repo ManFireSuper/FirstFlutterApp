53b914a5bd85e791a603e9128c907508f2953534
Manfiresuper <ekpreetsingh1st@gmail.com>
2023-01-12 16:14:44 +0530

    FIRST COMMIT

---
+++ first_app/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          body: Center(child: Text("This is a test field")),
+          appBar: AppBar(title: Text("New Title")))));
+}
