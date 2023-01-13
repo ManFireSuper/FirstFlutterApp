9e47d0bad87972d2450bf01e33817aa74ad2f1c2
Manfiresuper <ekpreetsingh1st@gmail.com>
2023-01-13 11:29:04 +0530

    Added ElevatedButtons

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
 
+// Day2_2
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
           body: Center(
-              child: Text("This is an text",
-                  style: TextStyle(color: Color(0xFFFF0000)))),
-          appBar: AppBar(title: Text("This is an title")))));
+              child: ElevatedButton(
+                  onPressed: () {
+                    print("Button is working");
+                  },
+                  child: Text("Press ME!"))),
+          appBar: AppBar(title: Text("My App Bar")),
+          floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                print("you are printing +");
+              },
+              child: Text("+")))));
 }
+// Notes :
+
+// Day2
+/*
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          body: Center(
+              child: Text("Hello World",
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 222, 24, 222),
+                      fontFamily: "EkpreetFont",
+                      fontSize: 30))),
+          appBar: AppBar(title: Text("My App Bar")))));
+}
+*/
+// Notes : TextStyle(color: Color.fromARGB(255, 222, 24, 222), fontWeight: FontWeight.bold, fontSize: 30)
+
+// Day1_2
+/*
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          body: Center(
+              child: Text("Hello World",
+                  style: TextStyle(color: Color.fromARGB(255, 222, 24, 222)))),
+          appBar: AppBar(title: Text("My App Bar")))));
+}
+*/
+// Notes : (color: Colors.red)
 
 // Day1
 /*
@@ -17,4 +56,5 @@ void main() {
           body: Center(child: Text("This is a test field")),
           appBar: AppBar(title: Text("New Title")))));
 }
+// Notes : NaN
 */
